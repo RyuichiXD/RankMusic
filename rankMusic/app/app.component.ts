4d93019ea8ff64b13c552618d9104fbb73e20641
Ryuichi001 <samed.sulanc@yahoo.de>
2016-11-02 19:28:24 +0100

    empty Project

---
+++ rankMusic/app/app.component.ts
@@ -0,0 +1,17 @@
+/**
+ * a sample component using the my-app directive
+ * @author Johannes Konert
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Heros</h1>'
+})
+export class AppComponent {
+
+  constructor() {
+
+  }
+}
+
